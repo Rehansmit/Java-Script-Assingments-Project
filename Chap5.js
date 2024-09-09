234c298bff2e1eb852694d5a38d02370dc6230cd
Rehan Shaikh <rehansmit724@gmail.com>
2024-09-09 09:12:19 -0700

    update chapter 5

---
+++ Chap5.js
@@ -0,0 +1,78 @@
+
+// Question -1
+
+var a = 20
+var b = 5
+
+var result = a + b
+
+document.write ("Sum of variable a & b" + "" + "" +result + "<br></br>");
+
+
+
+// Question -2
+
+var Subtract = a - b
+
+document.write ("Subract of variable a & b" + "" + ""+ Subtract + "<br></br>");
+
+
+var Multi = a * b
+
+document.write ("Multiply of variable a & b" + "" + "" + Multi + "<br></br>");
+
+
+var Divid = a / b
+
+document.write ("Division of variable a & b" + "" + "" + Divid + "<br></br>");
+
+
+var Mod = a % b
+
+document.write ("Modulus of variable a & b" + "" + "" + "" + Mod + "<br></br>");
+
+
+// Question -3
+
+// a
+var num;
+
+// b & C
+num = "??"
+
+// C
+
+num = 5
+
+// d
+
+document.write ("Initail Value" + "" + num + "<br>" + "<br>")
+
+// e
+
+++num 
+
+// f
+document.write ("Value after increment is" + num + "<br>" + "<br>")
+
+// g
+
+num
+
+// h
+
+document.write ("Value after addition is" +  num + "<br>" +"<br>")
+
+// i
+
+--num 
+
+// j
+document.write ("Value after decrement is" +  num + "<br>" +"<br>")
+
+
+
+
+
+
+

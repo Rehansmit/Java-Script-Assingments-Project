f7cd054532ab79b4304c16635ff8564c552e27eb
Rehan Shaikh <rehansmit724@gmail.com>
2024-09-09 12:41:33 -0700

    update question 6

--- Chap5.js
+++ Chap5.js
@@ -1,3 +1,6 @@
+document.write ("<b>"+ "Chapter No- 5 Assingments Answers"+ "</b>" + "<br>"+ "<hr>")
+
+
 
 // Question -1
 
@@ -5,15 +8,15 @@ var a = 20
 var b = 5
 
 var result = a + b
-document.write ("Answer Q.1" + "<br>")
-document.write ("Sum of variable a & b = " + " " + "" +result + "<br>"+"</br>");
+document.write ("Answer Q.1" + "<br>" +"<br>")
+document.write ("Sum of variable a & b = " + " " + "" +result + "<br>"+"<br>"+"<hr>");
 
 
 
 // Question -2
 
 var Subtract = a - b
-document.write ("Answer Q.2" + "<br>")
+document.write ("Answer Q.2" + "<br>"+"<br>")
 document.write ("Subract of variable a & b = " + " " + ""+ Subtract + "<br></br>");
 
 
@@ -29,7 +32,7 @@ document.write ("Division of variable a & b =  " + " " + " " + Divid + "<br></br
 
 var Mod = a % b
 
-document.write ("Modulus of variable a & b = " + "  " + " " + " " + Mod + "<br></br>");
+document.write ("Modulus of variable a & b = " + "  " + " " + " " + Mod + "<br></br>"+ "<hr>");
 
 
 // Question -3
@@ -45,7 +48,7 @@ num = "??"
 num = 5
 
 // d
-document.write ("Answer Q.3-d" + "<br>")
+document.write ("Answer Q.3-d" + "<br>"+"<br>")
 document.write ("Initail Value of variable num is" + "  " + num + "<br>" + "<br>")
 
 // e (incerement) 
@@ -79,14 +82,14 @@ num / 3
 
 // l
 document.write ("Answer Q.3-l" + "<br>")
-document.write ("Value after dividing by 3 is = " +"  "+  num%3 + "<br>" +"<br>")
+document.write ("Value after dividing by 3 is = " +"  "+  num%3 + "<br>" +"<br>"+"<hr>")
 
 
 // Question -4
 
 var ticket = 600;
-document.write ("Answer Q.4" + "<br>")
-document.write ("Cost of 5 movie tickets are = " +"  "+  ticket*5 + "  "+"Rupees" + "<br>" +"<br>")
+document.write ("Answer Q.4" + "<br>"+"<br>")
+document.write ("Cost of 5 movie tickets are = " +"  "+  ticket*5 + "  "+"Rupees" + "<br>" +"<hr>")
 
 
 // Question -5
@@ -94,7 +97,7 @@ document.write ("Cost of 5 movie tickets are = " +"  "+  ticket*5 + "  "+"Rupees
 document.write ("Answer Q.5" + "<br>" +  "<br>"+"<br>")
 var tvalue = +prompt ("Enter the value which table you required?");
 
-document.write ("<b>" + "Tables of "  + tvalue+ "<br></br>")
+document.write ("<b>"+"Tables of "  + tvalue+ "<br></br>")
 
 document.write (tvalue + "  "+" X "+ "1" + "  "+"=" +"  "+ tvalue*1 + "<br>" +"<br>")
 document.write (tvalue + "  "+" X "+ "2" + "  "+"=" +"  "+ tvalue*2 + "<br>" +"<br>")
@@ -105,14 +108,14 @@ document.write (tvalue + "  "+" X "+ "6" + "  "+"=" +"  "+ tvalue*6 + "<br>" +"<
 document.write (tvalue + "  "+" X "+ "7" + "  "+"=" +"  "+ tvalue*7 + "<br>" +"<br>")
 document.write (tvalue + "  "+" X "+ "8" + "  "+"=" +"  "+ tvalue*8 + "<br>" +"<br>")
 document.write (tvalue + "  "+" X "+ "9" + "  "+"=" +"  "+ tvalue*9 + "<br>" +"<br>")
-document.write (tvalue + "  "+" X "+ "10" + "  "+"=" +"  "+ tvalue*10 + "<br>" +"<br>"+"<br>")
+document.write (tvalue + "  "+" X "+ "10" + "  "+"=" +"  "+ tvalue*10 + "<br>" +"</b>"+"<hr>")
 
 
 // Question -6
 
 document.write ("Answer Q.6" + "<br>" + "<br>")
 
-document.write ("The Temperature Converter" + "<br>" + "<br>")
+document.write ("<b>"+"The Temperature Converter" + "<br>" + "<br>")
 
 var celsius = 25;
 var farnheit = 70;
@@ -121,9 +124,27 @@ var ctof = (celsius * 9 / 5)+32
 var ftoc = (farnheit - 32) * 5/ 9  
 
 document.write ("25 Celsius is ="+" "+ ctof+ " "+"NNoF " +"<br>" + "<br>")
-document.write ("70 Farenheit is ="+" "+ ftoc+ " "+"NNoC " +"<br>" + "<br>")
+document.write ("70 Farenheit is ="+" "+ ftoc+ " "+"NNoC " +"<br>"+"</b>" + "<hr>")
 
 
 // Question -7
 
 document.write ("Answer Q.7" + "<br>" + "<br>")
+document.write ("<b>"+"Shopping Cart System" + "<br>" + "<br>")
+
+var item1 = 650;
+var item2 = 100;
+var shipcharges = 100
+
+var tcost = (item1*3)+(item2*7)+shipcharges
+
+document.write ("Price of item1 is ............ ="+" "+ item1+ " "+"PKR " +"<br>")
+document.write ("cost of 3 item1 is 3 @ 650.............. ="+" "+ item1*3+ " "+"PKR " +"<br>")
+
+document.write ("Price of item2 is ............ ="+" "+ item2+ " "+"PKR " +"<br>")
+document.write ("cost of 7 item2 is 7 @ 100.............. ="+" "+ item2*7+ " "+"PKR " +"<br>")
+document.write ("Add Shipping Charges cost .............. ="+" "+ "@ 100 "+"PKR " +"<br>"+"<hr>")
+
+document.write ("Total Cost of Your order is ........... ="+" "+ tcost+ " "+"PKR " +"<br>"+"</b>" + "<hr>")
+
+

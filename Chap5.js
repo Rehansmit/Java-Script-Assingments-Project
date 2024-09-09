c59a04c489d0e97417f470606596c862bf04cbc4
Rehan Shaikh <rehansmit724@gmail.com>
2024-09-09 11:15:11 -0700

    update Question 6

--- Chap5.js
+++ Chap5.js
@@ -5,31 +5,31 @@ var a = 20
 var b = 5
 
 var result = a + b
-
-document.write ("Sum of variable a & b" + "" + "" +result + "<br></br>");
+document.write ("Answer Q.1" + "<br>")
+document.write ("Sum of variable a & b = " + " " + "" +result + "<br>"+"</br>");
 
 
 
 // Question -2
 
 var Subtract = a - b
-
-document.write ("Subract of variable a & b" + "" + ""+ Subtract + "<br></br>");
+document.write ("Answer Q.2" + "<br>")
+document.write ("Subract of variable a & b = " + " " + ""+ Subtract + "<br></br>");
 
 
 var Multi = a * b
 
-document.write ("Multiply of variable a & b" + "" + "" + Multi + "<br></br>");
+document.write ("Multiply of variable a & b = " + " " + " " + Multi + "<br></br>");
 
 
 var Divid = a / b
 
-document.write ("Division of variable a & b" + "" + "" + Divid + "<br></br>");
+document.write ("Division of variable a & b =  " + " " + " " + Divid + "<br></br>");
 
 
 var Mod = a % b
 
-document.write ("Modulus of variable a & b" + "" + "" + "" + Mod + "<br></br>");
+document.write ("Modulus of variable a & b = " + "  " + " " + " " + Mod + "<br></br>");
 
 
 // Question -3
@@ -37,7 +37,7 @@ document.write ("Modulus of variable a & b" + "" + "" + "" + Mod + "<br></br>");
 // a
 var num;
 
-// b & C
+// b
 num = "??"
 
 // C
@@ -45,34 +45,85 @@ num = "??"
 num = 5
 
 // d
+document.write ("Answer Q.3-d" + "<br>")
+document.write ("Initail Value of variable num is" + "  " + num + "<br>" + "<br>")
 
-document.write ("Initail Value" + "" + num + "<br>" + "<br>")
-
-// e
+// e (incerement) 
 
 ++num 
 
 // f
-document.write ("Value after increment is" + num + "<br>" + "<br>")
+document.write ("Answer Q.3-f" + "<br>")
+document.write ("Value after increment is ="+"  " + num + "<br>" + "<br>")
 
 // g
 
-num
+num=6+7
 
 // h
-
-document.write ("Value after addition is" +  num + "<br>" +"<br>")
+document.write ("Answer Q.3-h" + "<br>")
+document.write ("Value after addition 7 is = " +"  " +  num + "<br>" +"<br>")
 
 // i
 
 --num 
 
 // j
-document.write ("Value after decrement is" +  num + "<br>" +"<br>")
+document.write ("Answer Q.3-j" + "<br>")
+document.write ("Value after decrement is = " +"  "+  num + "<br>" +"<br>")
 
 
+// k
+
+num / 3 
+
+// l
+document.write ("Answer Q.3-l" + "<br>")
+document.write ("Value after dividing by 3 is = " +"  "+  num%3 + "<br>" +"<br>")
 
 
+// Question -4
+
+var ticket = 600;
+document.write ("Answer Q.4" + "<br>")
+document.write ("Cost of 5 movie tickets are = " +"  "+  ticket*5 + "  "+"Rupees" + "<br>" +"<br>")
 
 
+// Question -5
 
+document.write ("Answer Q.5" + "<br>" +  "<br>"+"<br>")
+var tvalue = +prompt ("Enter the value which table you required?");
+
+document.write ("<b>" + "Tables of "  + tvalue+ "<br></br>")
+
+document.write (tvalue + "  "+" X "+ "1" + "  "+"=" +"  "+ tvalue*1 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "2" + "  "+"=" +"  "+ tvalue*2 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "3" + "  "+"=" +"  "+ tvalue*3 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "4" + "  "+"=" +"  "+ tvalue*4 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "5" + "  "+"=" +"  "+ tvalue*5 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "6" + "  "+"=" +"  "+ tvalue*6 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "7" + "  "+"=" +"  "+ tvalue*7 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "8" + "  "+"=" +"  "+ tvalue*8 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "9" + "  "+"=" +"  "+ tvalue*9 + "<br>" +"<br>")
+document.write (tvalue + "  "+" X "+ "10" + "  "+"=" +"  "+ tvalue*10 + "<br>" +"<br>"+"<br>")
+
+
+// Question -6
+
+document.write ("Answer Q.6" + "<br>" + "<br>")
+
+document.write ("The Temperature Converter" + "<br>" + "<br>")
+
+var celsius = 25;
+var farnheit = 70;
+
+var ctof = (celsius * 9 / 5)+32  
+var ftoc = (farnheit - 32) * 5/ 9  
+
+document.write ("25 Celsius is ="+" "+ ctof+ " "+"NNoF " +"<br>" + "<br>")
+document.write ("70 Farenheit is ="+" "+ ftoc+ " "+"NNoC " +"<br>" + "<br>")
+
+
+// Question -7
+
+document.write ("Answer Q.7" + "<br>" + "<br>")

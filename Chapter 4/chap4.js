7721fd03a471615d137998d72bef821ded28ee34
Rehansmit <rehansmit724@gmail.com>
2024-09-11 16:28:01 +0500

    updated chapter 4

---
+++ Chapter 4/chap4.js
@@ -0,0 +1,40 @@
+
+//Question 1
+
+var num1 
+var num1
+var num1
+
+//Question 2
+// legal variables
+var uaerNmae
+var $myName
+var user_id
+var num1
+var mypassword
+
+//illegal variables
+
+// var user-name
+// var 2num
+// var my name
+// var 1234
+// var alert
+
+
+//Question-3
+
+//a
+document.write ( "<b>"+"Rules for naming JS variables" + "</b>"+ "<br>"+ "<br>");
+
+//b
+document.write ("Variable names can only contain LETTER, DOLLAR SIGNS, NUMBERS and UNDERSCORE. For example $my_1stVariable" + "<br>"+ "<br>");
+
+//c
+document.write ("Variables must begin with a letter, dollar sig or under score. For example $name, _name or name  ");
+
+//d
+document.write ("Variable names are case sensitive" + "</b>"+ "<br>"+ "<br>")
+
+//e
+document.write ("Variable names should not be JS keyword")   

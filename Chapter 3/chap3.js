2711fb610028955474ea668f90e5aa66f0eb65ff
Rehansmit <rehansmit724@gmail.com>
2024-09-11 14:44:04 +0500

    chapter has updated

---
+++ Chapter 3/chap3.js
@@ -0,0 +1,24 @@
+
+//Question 1
+
+var age = "15 years"
+alert("I am " + age +" "+ "old")
+
+//Question 2
+
+var vists = "14 times"
+alert("You have visited this site " +  vists)
+
+// //Question-3
+var birthyear = 1981
+
+alert("My Birth Years is " + birthyear)
+
+// //Question-4
+var vname = "Muhammad Ali"
+var product = "Cricket bat"
+var qty = 5
+
+alert(vname+ " " + "orderd "+" " + qty +" "+ product + " "+"from Shadman Store")
+
+

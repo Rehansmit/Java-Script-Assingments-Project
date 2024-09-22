753f6583821145dbfd235b28cc39cf3d9d4dbfb5
Rehan Shaikh <rehansmit724@gmail.com>
2024-09-22 11:16:28 -0700

    updated Q.5

---
+++ Chapter 12 to 13/app.js
@@ -0,0 +1,90 @@
+
+//************************Question No-1 **************************//
+//Write a program that takes a character (number or string) in a variable & checks whether the given input is 
+//anumber, uppercase letter or lower case letter. (Hint: ASCII codes:- A=65, Z=90, a=97, z=122).
+
+
+// var userinput = prompt("Please enter Text or Number ")
+
+// var result = userinput.charCodeAt()
+
+
+// if(result <= 57)
+//     {
+// alert("Your input text are number ")
+//  }
+// else if (result <= 90){
+// alert("Your input text are Upercase") }
+
+// else if (result <= 122 ){
+// alert("Your input text are lowercase") }
+
+// else{alert("enter correct value")}
+
+
+//**********************Question No-2 **************************//
+//Write a JavaScript program that accept two integers and display the larger. also show if the two integers are equal.
+
+
+// var num1 = +prompt("enter first integer or number")
+// var num2 = +prompt("enter 2nd integer or number")
+
+// if(num1 === num2){alert("Both numbers are equal")}
+// else if(num1 > num2) {alert("First number are greater than 2nd Number")}
+// else if(num1 < num2) {alert("2nd number are greater than 1st Number")}
+
+// else{alert("Enter correct number")}
+
+
+//***************************Question No-3 **************************//
+//Write a program that takes input a number from user & state whether the number is positive, negative or zero.
+
+
+// var unumber = +prompt("Please enter any number")
+
+// if(unumber===0){alert("input number is Zero")}
+
+// else if(unumber > 0){alert("input number is Positive")}
+// else if(unumber < 0){alert("input number is Nagative")}
+// else{("Enter correct number")}
+
+
+
+
+//***************************Question No-4 **************************//
+//Write a program that takes a character (i.e. string of length 1) and returns true if it is a vowel, false otherwise
+
+// var userletter = prompt("Please enter a letter")
+
+
+// if(userletter === "a" || userletter ==="e" || userletter === 'i' || userletter === "o" || userletter === "u"){alert( "Your letter is Vowel" )}
+// else {alert("false")}
+
+
+
+//***************************Question No-5 **************************//
+//Write a program that a. Store correct password in a JS variable.b. Asks user to enter his/her password
+//c. Validate the two passwords: i. Check if user has entered password. If not, then 
+//give message “ Please enter your password” ii. Check if both passwords are same. If they are 
+//same, show message “Correct! The password you entered matches the original password”. Show 
+//“Incorrect password” otherwise.
+
+
+
+var userPassword = "admin@mail.com"
+
+var inputPassword = prompt("Please enter your password")
+
+if(inputPassword == ""){
+    alert("Please enter your password")
+}
+
+else if (inputPassword != userPassword){alert("You enter wrong password")}
+else if (inputPassword === userPassword){alert("You entered correct password")}
+else{alert(" Please Enter Password")}
+
+
+
+
+
+

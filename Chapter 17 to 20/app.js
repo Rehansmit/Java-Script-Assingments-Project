f73f24a0fc1c1475612f14b4876bd3cc4ee92e52
Rehansmit <rehansmit724@gmail.com>
2024-10-03 19:08:35 +0500

    initiate

---
+++ Chapter 17 to 20/app.js
@@ -0,0 +1,9 @@
+
+//************************Question No-1 **************************//
+
+// Declare an empty array using JS literal notation to store student names in future. .
+
+
+// var studentName;
+
+

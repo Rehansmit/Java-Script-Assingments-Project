3eb602afefe40db70b5b8619614b2cd10ce1f67d
Rehan Shaikh <rehansmit724@gmail.com>
2024-10-15 09:59:38 -0700

    updating assignment form 21 to 25

---
+++ Chapter 21 to 25/app.js
@@ -0,0 +1,130 @@
+
+//************************Question No-1 **************************//
+// Write a program that takes two user inputs for first and 
+// last name using prompt and merge them in a new variable 
+// titled fullName. Greet the user using his full name.
+
+
+// var firstNmae = prompt('please enter your furst name')
+// var lastNmae = prompt('please enter your last name')
+
+// var fullNmae = firstNmae + lastNmae
+
+// alert('Welcome Mr.' + fullNmae)
+
+
+//************************Question No-2 **************************//
+
+// Write a program to take a user input about his favorite 
+// mobile phone model. Find and display the length of user 
+// // input in your browser
+
+
+// var mobileModel = prompt('please enter your favorate mobile name & model ')
+// var length = mobileModel.length
+// document.write(`Your Favorate mobile name and model is' ${mobileModel} <br> Length of string ${length}`)
+
+
+//************************Question No-3 **************************//
+// Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+
+
+// var cityNmae = 'Pakistani'
+
+// var indexNum = cityNmae.indexOf('n')
+// document.write(`String Value ${cityNmae} <br> index number of 'n' is ${indexNum}`)     
+
+
+
+//************************Question No-4 **************************//
+// Write a program to find the last index of letter “l” in the 
+// word “Hello World” and display the result in your browser.
+
+
+
+// var word = "Hello World"
+// var indexNum = word.lastIndexOf('l')
+// document.write(`String Name ${word} <br> last index number of 'l' is ${indexNum}`)     
+
+
+
+//************************Question No-5 **************************//
+// Write a program to find the character at 3rd index in the 
+// word “Pakistani” and display the result in your browser.
+
+
+// var cityNmae = 'Pakistani'
+
+// var index3rd = cityNmae.charAt(3)
+// alert("Letter of 3rd index is" + index3rd)
+
+
+
+
+//************************Question No-6 **************************//
+// 6. Repeat Q1 using string concat() method.
+
+
+
+
+//************************Question No-7 **************************//
+// Write a program to replace the “Hyder” to “Islam” in the 
+// word “Hyderabad” and display the result in your browser.
+
+// var cityFirst = prompt("Enter the first city name")
+// alert("you enter " + cityFirst)
+
+// var updatecity = prompt("enter city name which you need to repalce")
+
+// var repalceCity= cityFirst.replaceAll(cityFirst, updatecity)
+// alert("You replaced"     +cityFirst+   'with  '   +   updatecity +    "now update city is"     +   repalceCity)
+
+
+
+
+//************************Question No-8 **************************//
+// Write a program to replace all occurrences of “and” in the
+// string with “&” and display the result in your browser.
+// var message = “Ali and Sami are best friends. They play cricket and 
+// football together.”;
+
+
+// var message = "Ali and Sami are best friends. They play cricket and football together"
+// var updateTex = message.replaceAll("and" , "&")
+// alert(updateTex)
+
+
+//************************Question No-9 **************************//
+// Write a program that converts a string “472” to a number 
+// 472. Display the values & types in your browser.
+
+
+// var stringNum = "472"
+// var changeNum = stringNum.replaceAll("472", 472 )
+// document.write(`Valu = ${stringNum} <br> Data type = String <br> value ${changeNum} <br> Data type is number`)
+
+
+
+// //************************Question No-10 **************************//
+// Write a program that takes user input. Convert and 
+// show the input in capital letters
+
+
+// var inputword = prompt('enter any word in small letter ')
+// var updateWord = inputword.toUpperCase()
+// alert("Upper Case of your input word is " + updateWord)
+
+
+// //************************Question No-11 **************************//
+// Write a program that takes user input. Convert and 
+// show the input in title case.
+
+
+// var inputword = prompt('enter any word ')
+// var titleletter = inputword.charAt(0).toLocaleUpperCase()
+// var remainingWord = inputword.slice(1)
+// var updateTital = titleletter+remainingWord 
+// alert(updateTital)
+
+

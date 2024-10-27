a23cb6ce0e51c93f3ff68dcb68382a455ce32bff
Rehansmit <rehansmit724@gmail.com>
2024-10-27 13:47:08 +0500

    update 18 question

---
+++ Password generator/app.js
@@ -0,0 +1,8 @@
+var aBC  = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"
+var abc  = "abcdefghijklmnopqrstuvwxyz"
+var a123 = "1234567890"
+var a$$ = "~!@#$%^&*()_-"
+
+var randomPassword = ""
+
+var charlist = Math.floor(Math.random()*aBC.length+1)

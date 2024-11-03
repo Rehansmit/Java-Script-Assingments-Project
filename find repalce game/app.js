cdb1cb8371d186a6f837f1c86d97c18ae4a23353
Rehan Shaikh <rehansmit724@gmail.com>
2024-11-03 07:40:22 -0800

    find

---
+++ find repalce game/app.js
@@ -0,0 +1,21 @@
+
+var input_tex = prompt('Please enter a small paragraph')
+document.write (`<h3> Your Input Text Are </h3> <br><br> <p>${input_tex} `)
+
+var option = +prompt("Please slect 1 if you want to find any thing \n or slect 2 if you want to find and replace thing ")
+var indexnum;
+var update;
+
+if(option===1){var find_word = prompt("please enter word do you want to find")
+    
+for(i=0; i<input_tex.length; i++)
+    {if(find_word===input_tex[i]){update=input_tex[i] , indexnum = i}}}
+
+else if(option===2){var replace_word = prompt("Please enter new word do you want to repl
+    *9+
+  7 . ace")}
+    else{alert('enter correct option')}
+    
+}
+
+
